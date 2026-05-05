b1919e48677e9963ef8dece144722488ed578c78
Karol-Bobryk <bobrykkarol04@gmail.com>
2026-05-05 22:52:15 +0200

    Init commit

---
+++ src/main/java/com/example/cheiftain/CheiftainApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheiftain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheiftainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheiftainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cheiftain/CheiftainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheiftain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheiftainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
