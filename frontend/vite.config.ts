7dc23b3882d08ff152c2b33d55e9ede58f22db24
Karol-Bobryk <bobrykkarol04@gmail.com>
2026-05-20 15:06:10 +0200

    added quotes around port numbers and turned on vite hosting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 3000,
   },
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
