120da30b6fa8a1d2e13377e800660e8f097a37b6
Karol-Bobryk <bobrykkarol04@gmail.com>
2026-05-31 23:23:58 +0200

    added user creation

--- backend/src/main/java/com/chieftain/repositories/UserRepository.java
+++ backend/src/main/java/com/chieftain/repositories/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
   Optional<UserEntity> findByEmailAddress(String emailAddress);
+  boolean existsByEmailAddress(String emailAddress);
 
   boolean existsByEmailAddress(String emailAddress);
 }
