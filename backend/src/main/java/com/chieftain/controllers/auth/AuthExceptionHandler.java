a510bfa68d2349c67b5fac9691ab43cc7829de25
Karol-Bobryk <bobrykkarol04@gmail.com>
2026-06-03 21:38:52 +0200

    added better error response body

--- backend/src/main/java/com/chieftain/controllers/auth/AuthExceptionHandler.java
+++ backend/src/main/java/com/chieftain/controllers/auth/AuthExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.chieftain.controllers.auth;
 
+import com.chieftain.controllers.auth.dto.ErrorResponseDTO;
 import com.chieftain.exceptions.EmailAddressNotFoundException;
 import com.chieftain.exceptions.EmailIsAlreadyTakenException;
 import com.chieftain.exceptions.InvalidUserSecretProvidedException;
@@ -14,44 +15,46 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.chieftain.controllers.auth")
 public class AuthExceptionHandler {
-  // TODO: we should consider returning JSON body with a message field instead of this brutish raw
-  // text
-
   @ExceptionHandler(EmailIsAlreadyTakenException.class)
-  public ResponseEntity<String> handleEmailTaken() {
-    return ResponseEntity.status(HttpStatus.CONFLICT).body("Email address is already in use");
+  public ResponseEntity<ErrorResponseDTO> handleEmailTaken() {
+    HttpStatus status = HttpStatus.CONFLICT;
+    return ResponseEntity.status(status).body(ErrorResponseDTO.of(status,"Email address is already in use"));
   }
 
   @ExceptionHandler(UserSecretNotProvidedException.class)
-  public ResponseEntity<String> handleSecretNotProvided() {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Secret not provided");
+  public ResponseEntity<ErrorResponseDTO> handleSecretNotProvided() {
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    return ResponseEntity.status(status).body(ErrorResponseDTO.of(status,"Secret not provided"));
   }
 
   @ExceptionHandler(InvalidUserSecretProvidedException.class)
-  public ResponseEntity<String> handleInvalidSecret() {
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid user secret provided");
+  public ResponseEntity<ErrorResponseDTO> handleInvalidSecret() {
+    HttpStatus status = HttpStatus.UNAUTHORIZED;
+    return ResponseEntity.status(status).body(ErrorResponseDTO.of(status,"Invalid user secret provided"));
   }
 
   @ExceptionHandler(EmailAddressNotFoundException.class)
-  public ResponseEntity<String> handleEmailNotFound() {
-    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-        .body("There is no matching user for given email address");
+  public ResponseEntity<ErrorResponseDTO> handleEmailNotFound() {
+    HttpStatus status = HttpStatus.UNAUTHORIZED;
+    return ResponseEntity.status(status)
+        .body(ErrorResponseDTO.of(status,"There is no matching user for given email address"));
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   @ResponseStatus()
-  public ResponseEntity<String> handleValidation(MethodArgumentNotValidException ex) {
-
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(
+  public ResponseEntity<ErrorResponseDTO> handleValidation(MethodArgumentNotValidException ex) {
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    return ResponseEntity.status(status)
+        .body(ErrorResponseDTO.of(status,
             "Request field validation failed "
                 + Objects.requireNonNull(ex.getBindingResult().getFieldError())
-                    .getDefaultMessage());
+                    .getDefaultMessage()));
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleGeneralAuthError(Exception ex) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body("Unknown authentication error occurred: " + ex.getMessage());
+  public ResponseEntity<ErrorResponseDTO> handleGeneralAuthError(Exception ex) {
+    HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+    return ResponseEntity.status(status)
+        .body(ErrorResponseDTO.of(status,"Unknown authentication error occurred: " + ex.getMessage()));
   }
 }
