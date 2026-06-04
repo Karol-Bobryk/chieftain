5db1f929966e7f372a3df106e7f0d4a15555159a
Karol-Bobryk <bobrykkarol04@gmail.com>
2026-06-04 13:32:46 +0200

    organized public paths

--- backend/src/main/java/com/chieftain/configs/SecurityConfig.java
+++ backend/src/main/java/com/chieftain/configs/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
   // TODO: ADD ROLES ENUM
+  private final String[] publicPaths = {"/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"};
+
   private final CustomUserDetailsService customUserDetailsService;
 
   @Autowired
@@ -35,9 +37,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             authorize ->
                 authorize
-                    .requestMatchers("/auth/**")
-                    .permitAll()
-                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                    .requestMatchers(publicPaths)
                     .permitAll()
                     .anyRequest()
                     .authenticated())
