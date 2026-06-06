82d2f0d3002e39a82155ab33046735e49c58ed48
Karol-Bobryk <bobrykkarol04@gmail.com>
2026-06-06 23:34:42 +0200

    removed unused imports

--- backend/src/main/java/com/chieftain/exceptions/UserNotFoundInQueueException.java
+++ backend/src/main/java/com/chieftain/exceptions/UserNotFoundInQueueException.java
@@ -1,7 +1,6 @@
 package com.chieftain.exceptions;
 
 import org.jspecify.annotations.Nullable;
-import org.springframework.security.core.AuthenticationException;
 
 public class UserNotFoundInQueueException extends RuntimeException{
   public UserNotFoundInQueueException(@Nullable String msg) {
