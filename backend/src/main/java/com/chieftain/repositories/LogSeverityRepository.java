85dbba3ac321c9ec7164b012f544b8ee853055c7
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-06-07 15:36:31 +0200

    add findByLogSeverityName to repository

--- backend/src/main/java/com/chieftain/repositories/LogSeverityRepository.java
+++ backend/src/main/java/com/chieftain/repositories/LogSeverityRepository.java
@@ -1,6 +1,11 @@
 package com.chieftain.repositories;
 
+import com.chieftain.enums.LogSeverity;
 import com.chieftain.models.LogSeverityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LogSeverityRepository extends JpaRepository<LogSeverityEntity, Integer> {}
+import java.util.Optional;
+
+public interface LogSeverityRepository extends JpaRepository<LogSeverityEntity, Integer> {
+    Optional<LogSeverityEntity> findByLogSeverityName(LogSeverity severityName);
+}
