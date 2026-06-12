fcad02eb1335b251b1e503a66816ee85185134e2
Anna Pienkowska <a.pienkowska3@gmail.com>
2026-06-13 00:32:33 +0200

    update controller and service to use AdminLogJdbcRepository

--- backend/src/main/java/com/chieftain/services/AdminLogService.java
+++ backend/src/main/java/com/chieftain/services/AdminLogService.java
@@ -1,6 +1,8 @@
 package com.chieftain.services;
 
+import com.chieftain.controllers.admin.dto.SystemLogDTO;
 import com.chieftain.models.SystemLogViewEntity;
+import com.chieftain.repositories.AdminLogJdbcRepository;
 import com.chieftain.repositories.SystemLogViewRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,14 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminLogService {
 
-  private final SystemLogViewRepository systemLogViewRepository;
+  private final AdminLogJdbcRepository adminLogJdbcRepository;
 
-  public AdminLogService(SystemLogViewRepository systemLogViewRepository) {
-    this.systemLogViewRepository = systemLogViewRepository;
+  public AdminLogService(AdminLogJdbcRepository adminLogJdbcRepository) {
+    this.adminLogJdbcRepository = adminLogJdbcRepository;
   }
 
-  public Page<SystemLogViewEntity> getSystemLogs(Pageable pageable) {
+  public Page<SystemLogDTO> getSystemLogs(String domain, String severity, Pageable pageable){
 
-    return systemLogViewRepository.findAll(pageable);
+    return adminLogJdbcRepository.findFilteredLogs(domain, severity, pageable);
   }
 }
