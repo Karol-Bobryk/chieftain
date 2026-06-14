5fe3b711709526aed1016c0df43a61e90dd86f8d
Abhain <91952@student.pb.edu.pl>
2026-06-14 14:26:07 +0200

    Fix trigger

---
+++ backend/src/main/resources/db/migration/V11__fix_task_done_at_trigger.sql
@@ -0,0 +1,27 @@
+DROP TRIGGER IF EXISTS trg_task_done_at ON tasks;
+
+CREATE OR REPLACE FUNCTION set_task_done_at() RETURNS TRIGGER AS $task_stat$
+
+DECLARE
+finished_status_id INTEGER;
+BEGIN
+SELECT pk_task_status_id INTO finished_status_id
+FROM task_status_dictionary
+WHERE status_name = 'FINISHED';
+
+IF NEW.fk_status_id = finished_status_id
+      THEN
+        IF NEW.done_at IS NULL
+        THEN
+            NEW.done_at = NOW();
+END IF;
+ELSE
+        NEW.done_at = null;
+END IF;
+RETURN NEW;
+END;
+
+$task_stat$ LANGUAGE plpgsql;
+CREATE TRIGGER trg_task_done_at
+    BEFORE INSERT OR UPDATE OF fk_status_id ON tasks
+    FOR EACH ROW EXECUTE FUNCTION set_task_done_at();
