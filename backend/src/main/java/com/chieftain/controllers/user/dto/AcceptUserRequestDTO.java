2865da292d9845424e8ea785b2e255bfbd10e484
Karolina Dec <karolina.dec.001@gmail.com>
2026-06-18 20:18:49 +0200

    notnull

--- backend/src/main/java/com/chieftain/controllers/user/dto/AcceptUserRequestDTO.java
+++ backend/src/main/java/com/chieftain/controllers/user/dto/AcceptUserRequestDTO.java
@@ -2,11 +2,13 @@ package com.chieftain.controllers.user.dto;
 
 import com.chieftain.enums.SystemRole;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AcceptUserRequestDTO {
+  @NotNull
   private SystemRole role;
 }
