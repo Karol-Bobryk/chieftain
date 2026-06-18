a0c3c7bc553d95577e44cdd08fa801206c1c28c3
Abhain <91952@student.pb.edu.pl>
2026-06-19 00:24:09 +0200

    Added admin panel

--- backend/src/main/java/com/chieftain/repositories/UserRepository.java
+++ backend/src/main/java/com/chieftain/repositories/UserRepository.java
@@ -11,6 +11,8 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmailAddress(String emailAddress);
@@ -24,4 +26,9 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     SecretHashOnly findSecretHashByEmailAddress(String emailAddress);
 
     Page<UserEntity> findByOrganization(OrganizationEntity organization, Pageable pageable);
+
+    @Query("SELECT u FROM UserEntity u WHERE " +
+        "LOWER(CONCAT(u.name, ' ', u.surname)) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+        "LOWER(CONCAT(u.surname, ' ', u.name)) LIKE LOWER(CONCAT('%', :search, '%'))")
+    List<UserEntity> searchByFullName(@Param("search") String search, Pageable pageable);
 }
